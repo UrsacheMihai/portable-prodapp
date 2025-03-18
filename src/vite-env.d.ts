a172f09c24c69732c70e3ecb1e8b49f5bf040444
DESKTOP-I176V4D\Mihai <0ursachemihaiandrei0@gmail.com>
2025-03-18 19:43:16 +0200

    #4

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-    plugins: [react()],
-    base: '/portable-prodapp/',
-    optimizeDeps: {
-        exclude: ['lucide-react'],
-    },
-});
